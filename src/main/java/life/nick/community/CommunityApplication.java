a6c8a75e407f867d5210d0e69bad52eb86be30b9
nick <jing826241@126.com>
2019-06-19 07:34:59 +0800

    init

---
+++ src/main/java/life/nick/community/CommunityApplication.java
@@ -0,0 +1,14 @@
+package life.nick.community;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+//启动项目的类
+public class CommunityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommunityApplication.class, args);
+    }
+
+}
